84714dfcab1d8533e10b59c34215efe3e0e6918c
vaniararaandini03 <vania24ti@mahasiswa.pcr.ac.id>
2025-12-11 15:55:25 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StaffController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+Route::get('/dashboard', function() {
+    return view('dashboard');
+});
